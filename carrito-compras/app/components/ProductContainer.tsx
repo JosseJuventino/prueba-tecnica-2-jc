87fa64139279cbd138cf1900cf850d0c3c595e2c
JosseJuventino <00048322@uca.edu.sv>
2025-03-26 08:40:51 -0600

    feat:Initialize project
    Initialize project using Next.js

---
+++ carrito-compras/app/components/ProductContainer.tsx
@@ -0,0 +1,6 @@
+export default function ProductContainer() {
+    
+  return <div className="grid grid-cols-5 gap-4">
+
+  </div>;
+}
---
+++ carrito-compras/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center text-3xl mt-2">Carrito de compras</h1>
+    </div>
+  );
+}
